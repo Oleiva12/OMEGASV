52f8d2211cd60d74deb73ee1b4441fc4204f63b8
Emerson21983 <emer21983@gmail.com>
2024-07-29 22:07:36 -0600

    Add tables for products, branches, and product_branches

--- DB/database.sql
+++ DB/database.sql
@@ -4,8 +4,20 @@ USE inventario;
 
 CREATE TABLE productos (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  descripcion VARCHAR(255) NOT NULL,
-  cantidad_en_stock INT NOT NULL,
-  precio_compra DECIMAL(10, 2) NOT NULL,
-  fecha_vencimiento DATE,
+  nombre VARCHAR(255) NOT NULL,
+  descripcion VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE sucursales (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE productos_sucursales (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_producto INT NOT NULL,
+  id_sucursal INT NOT NULL,
+  cantidad INT NOT NULL,
+  FOREIGN KEY (id_producto) REFERENCES productos(id),
+  FOREIGN KEY (id_sucursal) REFERENCES sucursales(id)
 );
